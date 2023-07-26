6ccf1078bf33a62c386737c8979f276a777f0b17
Tyler Oakleaf <toakleaf@kayak.com>
2023-07-26 11:01:25 -0400

    Updated astronvim and changed theme to gruvbox

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,13 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim", enabled = false },
+  { import = "astrocommunity.colorscheme.gruvbox-baby", enabled = false },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- {
+  --   "gruvbox-baby",
+  --   opts = {
+  --     background_color = "dark",
+  --   },
+  -- },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,41 @@
 return {
-    -- You can also add new plugins here as well:
-    -- Add plugins, the lazy syntax
-    -- "andweeb/presence.nvim",
-    -- {
-    --   "ray-x/lsp_signature.nvim",
-    --   event = "BufRead",
-    --   config = function()
-    --     require("lsp_signature").setup()
-    --   end,
-    -- },
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    config = function()
+      require("gruvbox").setup {
+
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "hard", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = true,
+      }
+    end,
+  },
 }
